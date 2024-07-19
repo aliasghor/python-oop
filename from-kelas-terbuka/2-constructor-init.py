d655c3c4a656a7627c1f7d5ba16625e34aaf522b
aliasghor <aliasghor6@gmail.com>
2024-07-19 22:18:20 +0700

    Commited my 2-constructor.py file from-kelas-terbuka branch.

---
+++ from-kelas-terbuka/2-constructor-init.py
@@ -0,0 +1,21 @@
+# 2. Python constructor __init__ tutorial
+
+# What is constructor __init__ used for?
+# Constructor __init__ is used to initalize an instance's or object's attribute value implicitly using the `self` parameter.
+# `self` refers to an object of the class. Currently in this code `self` refers to hero1 and hero2 objects.
+
+# The `class` keyword is used to group every method and attributes together to definew a new type of object (instance). 
+class Hero:
+    def __init__(self, name: str, health: int, power: int, armor: int) -> None:
+        # Initalize the object's attribute value to each objects.
+        self.name = name
+        self.health = health
+        self.power = power
+        self.armor = armor
+
+# Instantiate the instances object
+hero1 = Hero("Gerry",100,10,30)
+hero2 = Hero("Mogi",100,30,10)
+
+print(hero1.__dict__)
+print(hero2.__dict__)
